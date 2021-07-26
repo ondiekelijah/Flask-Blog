cc1330a884d61fb7beeb63572be1288348db947a
ONDIEK ELIJAH OCHIENG <57575260+Dev-Elie@users.noreply.github.com>
2021-07-26 21:49:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ search = Search()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "67eadccda3bc198fangelus"
+    app.secret_key = "secret-key"
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = "mysql+mysqlconnector://elie:dev123@localhost/blog"
+    ] = "mysql+mysqlconnector://user_name:pwd@localhost/database_name"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config['INFOKIT_ADMIN'] = "eochieng9448@gmail.com"
+    app.config['INFOKIT_ADMIN'] = "*********@gmail.com"
 
     login_manager.init_app(app)
     with app.app_context():
@@ -78,42 +78,3 @@ def create_app():
     return app
 
 
-# Use this to insert db roles
-# Uncomment lines 80- 105 when inserting roles,comment the above
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_bcrypt import Bcrypt
-# from flask_login import LoginManager
-
-# login_manager = LoginManager()
-# login_manager.session_protection = "strong"
-# login_manager.login_view = "auth.login"
-# login_manager.login_message_cartegory = "info"
-# db =SQLAlchemy()
-# bcrypt = Bcrypt()
-
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.secret_key = '67eadccda3bc198fangelus'
-#     app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://elie:dev123@localhost/blog'
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#     login_manager.init_app(app)
-
-#     bcrypt.init_app(app)
-#     db.init_app(app)
-
-#     return app
-
-# Inserting User roles
-# Use these lines to avaoid "out of context" errors
-# from app import create_app
-# app = create_app()
-# app.app_context().push()
-# from models import Role
-# from app import db
-# Role.insert_roles()
-# Role.query.all()
-
-# [<Role 'User'>, <Role 'Moderator'>, <Role 'Administrator'>]
